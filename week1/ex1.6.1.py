f85eb6e8a5742b5048218bc24c3d0a5424e0ae03
unknown <dinhhuy25102004@gmail.com>
2025-05-17 11:22:47 +0700

    tuan1

---
+++ week1/ex1.6.1.py
@@ -0,0 +1,4 @@
+print("Hello, World!")
+print("My name is Nhu Dinh Huy")
+print("HUTECH University")
+
---
+++ week1/ex_1.6.2_03.py
@@ -0,0 +1,5 @@
+so = int(input("Nhap mot so nguyen: "))
+if so %2 == 0:
+    print (so, "La so chan")
+else:
+    print (so, "La so le")
