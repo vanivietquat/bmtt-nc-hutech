6550b5d44cb451cb97ae5c0dd27eea1c6c29a7ba
vanivietquat <dinhuy25102004@gmail.com>
2025-05-24 00:56:03 +0700

    Cuoi Cung cung xong

--- week1/1.6.4/QuanLySinhVien.py
+++ week1/1.6.4/QuanLySinhVien.py
@@ -44,3 +44,60 @@ class QuanLySinhVien:
             sv._major = major
             sv._diemTB = diemTB
             self.xepLoaiHocLuc(sv)
+        else:
+            print("Sinh vien co ID = {} khong ton tai." .format(ID))
+            
+    def sortByID(self):
+        self.listSinhVien.sort(key=lambda x: x._id, reverse= False)
+    
+    def sortByName(self):
+        self.listSinhVien.sort(key=lambda x: x._id, reverse= False)
+        
+    def sortByDiem(self):
+        self.listSinhVien.sort(key=lambda x: x._id, reverse= False)
+        
+    def findByID(self, ID):
+        listSV = []
+        if( self.soLuongSinhVien() > 0):
+            for sv in self.listSinhVien:
+                if(sv._id == ID):
+                    searchResult = sv
+        return searchResult
+    
+    def findByName(self, keyword):
+        listSV = []
+        if (self.soluongSinhVien() > 0):
+            for sv in self.listSinhVien:
+                if (keyword.upper() in sv._name.upper()): listSV.append(sv)
+        return listSV
+
+    def deleteById(self, ID):
+        isDeleted = False
+        sv = self.findByID(ID)
+        if (sv != None):
+            self.listSinhVien.remove(sv)
+            isDeleted  = True
+        return isDeleted
+
+    def xepLoaiHocLuc(self, sv:SinhVien):
+        if (sv._diemTB >= 8):
+            sv_hocLuc = "Gioi"
+        elif (sv._diemTB >= 6.5):
+            sv. hocLuc = "Kha"
+        elif (sv._diemTB >= 5):
+            sv_hocLuc = "Trung bình"
+        else:
+            sv_hocLuc = "Yeu"
+
+    def showSinhVien(self, listSV):
+        print("{:<8} {:<18} {:<8} {:<8} {:<8} {:<8}".format("ID", "Name", "Sex", "Major", "Diem TB", "HocLuc"))
+        if (listSV.__len__() > 0):
+            for sv in listSV:
+                print("{:<8} {:<18} {:<8} {:<8} {:<8} {:<8}".
+                      format(sv._id, sv._name, sv._sex, sv._major, sv._diemTB, sv._hocLuc))
+        print("\n")
+
+    def getListSinhVien(self):
+        return self.listSinhVien
+        
+    
--- week1/1.6.4/SinhVien.py
+++ week1/1.6.4/SinhVien.py
@@ -2,7 +2,7 @@ class SinhVien:
     def __init__(self, id, name, sex, major, diemTB):
         self._id = id
         self._name = name
-        self. sex = sex
+        self._sex = sex
         self._major = major
         self._diemTB= diemTB
         self._hocLuc = ""
